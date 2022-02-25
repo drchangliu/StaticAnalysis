3debd81ac786e1332d85174225092129f3d56dcc
drchangliu <drchangliu@users.noreply.github.com>
2022-02-25 10:59:22 -0500

    Update file3.cc

--- file3.cc
+++ file3.cc
@@ -1,12 +1,12 @@
-void f1(struct fred_t *p)
+void function1(struct student_record_t *p)
 {
     // dereference p and then check if it's NULL
-    int x = p->x;
+    int num = p->num;
     if (p)
-        do_something(x);
+        check_student_num(num);
 }
 
-void f2()
+void function2()
 {
     const char *p2 = NULL;
     for (int i = 0; str[i] != '\0'; i++)
@@ -18,17 +18,17 @@ void f2()
         }
     }
 
-    // p is NULL if str doesn't have a space. If str always has a
+    // p2 is NULL if str doesn't have a space. If str always has a
     // a space then the condition (str[i] != '\0') would be redundant
     return p[1];
 }
 
-void f3(int a)
+void function3(int a)
 {
-    struct fred_t *p3 = NULL;
-    if (a == 1)
-        p3 = fred1;
+    struct student_record_t *p3 = NULL;
+    if (flag == 1)
+        p3 = student_record_p_1;
 
-    // if a is not 1 then p is NULL
-    p3->x = 0;
+    // if flag is not 1 then p3 is NULL
+    p3->num = 0;
 }
